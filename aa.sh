e813bded76b1e8653234bfe45d8c7b2d27d44274
Your Name <juneja.namit@gmail.com>
2016-01-05 21:20:04 +0530

    code clean up

--- aa.sh
+++
@@ -1,3 +0,0 @@
-nmcli nm wifi on
-echo $?
-nmcli c up id VIT2.4G
--- run.sh
+++
@@ -1,7 +0,0 @@
-curl -i \
--H "Content-Type: application/x-www-form-urlencoded" \
--X POST --data '{,"userId":"junejanamit","password":"orangearmour","serviceName":"ProntoAuthentication","Submit22":"Login"}' "http://phc.prontonetworks.com/cgi-bin/authlogin?URI=http://go.microsoft.com/fwlink/"
-
-
-curl -s --data-urlencode 'userId'="junejanamit" --data-urlencode 'Submit22'='Login' --data-urlencode 'serviceName'='ProntoAuthentication' --data-urlencode 'password'="orangearmour" "http://phc.prontonetworks.com/cgi-bin/authlogin"
-
